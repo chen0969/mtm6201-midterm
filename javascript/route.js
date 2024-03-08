85b55550b173dfafc056940b3c3bb1a7b20f9521
Canada-Broccoli <chen0969@algonquinlive.com>
2024-03-08 15:12:13 -0500

    add rwd

--- javascript/route.js
+++ javascript/route.js
@@ -1,3 +1,12 @@
 function backHome(){
     window.location.href = "index.html";
 }
+
+function toFav(){
+    window.location.href = "fav.html";
+}
+
+function toAcc(){
+    window.location.href = "acc.html";
+}
+
